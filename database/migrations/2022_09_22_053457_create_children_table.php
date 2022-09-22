b37285b9ef671f03a32cb9222edffeb8bf769ddb
mots-and-code <hacmotshiro2@yahoo.co.jp>
2022-09-22 14:53:05 +0900

    laravel初期＋migration

---
+++ database/migrations/2022_09_22_053457_create_children_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('children', function (Blueprint $table) {
+            $table->collation = 'utf8mb4_general_ci';
+
+            $table->id();
+            $table->string('name')->nullable();
+            $table->string('userId')->nullable();
+            $table->string('child')->nullable();
+
+            // $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('children');
+    }
+};
